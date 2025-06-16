8ba0b7bcf857586a057c75bab144b3108bbda53f
AniketMGawade95 <aniketmgawade2019@gmail.com>
2025-06-16 14:06:51 +0530

    medicine updatadet

--- PharmaAPICreation/PharmaAPICreation/Migrations/20250616083540_medicineupdated.Designer.cs
+++ PharmaAPICreation/PharmaAPICreation/Migrations/20250616083540_medicineupdated.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PharmaAPICreation.Data;
 
@@ -11,9 +12,11 @@ using PharmaAPICreation.Data;
 namespace PharmaAPICreation.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250616083540_medicineupdated")]
+    partial class medicineupdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PharmaAPICreation/PharmaAPICreation/Migrations/20250616083540_medicineupdated.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PharmaAPICreation.Migrations
+{
+    /// <inheritdoc />
+    public partial class medicineupdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SaleItems_Medicines_MedicineId",
+                table: "SaleItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SaleItems_MedicineId",
+                table: "SaleItems");
+
+            migrationBuilder.DropColumn(
+                name: "MedicineId",
+                table: "SaleItems");
+
+            migrationBuilder.DropColumn(
+                name: "ExpiryDate",
+                table: "Medicines");
+
+            migrationBuilder.DropColumn(
+                name: "Manufacturer",
+                table: "Medicines");
+
+            migrationBuilder.DropColumn(
+                name: "PricePerUnit",
+                table: "Medicines");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Medicines");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MedicineId",
+                table: "SaleItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpiryDate",
+                table: "Medicines",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Manufacturer",
+                table: "Medicines",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PricePerUnit",
+                table: "Medicines",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Medicines",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleItems_MedicineId",
+                table: "SaleItems",
+                column: "MedicineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SaleItems_Medicines_MedicineId",
+                table: "SaleItems",
+                column: "MedicineId",
+                principalTable: "Medicines",
+                principalColumn: "MedicineId");
+        }
+    }
+}
