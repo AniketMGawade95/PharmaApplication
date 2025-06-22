b8264ae12d9afa5b48841d75e989dd09012302f1
Mulayam Yadav <146064973+mulayam-my18@users.noreply.github.com>
2025-06-22 21:12:10 +0530

    my2

--- PharmaAPIConsuming/PharmaAPIConsuming/DTO/CustomerDTO.cs
+++ PharmaAPIConsuming/PharmaAPIConsuming/DTO/CustomerDTO.cs
@@ -7,5 +7,9 @@
         public string Name { get; set; }
         public string Mobile { get; set; }
         public string Address { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
--- PharmaAPICreation/PharmaAPICreation/Program.cs
+++ PharmaAPICreation/PharmaAPICreation/Program.cs
@@ -1,9 +1,12 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore; // Ensure this namespace is includedusing PharmaAPICreation.Data;
+using Microsoft.IdentityModel.Tokens;
 using PharmaAPICreation.Data;
 using PharmaAPICreation.Mapper;
 using PharmaAPICreation.Repo;
 using PharmaAPICreation.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,6 +44,32 @@ builder.Services.AddScoped<IMedicineRepository, MedicineService>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<CustomerRepo, CustomerServices>();
 
+//Add JWT Authentication Configuration
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
+//======================================
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -57,6 +86,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
